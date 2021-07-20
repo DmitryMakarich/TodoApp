28fe6b7768e4e1dfb11184b2116d3fdfc0958d47
DmitryMakarich <dima.makarich@mail.ru>
2021-07-21 00:18:18 +0300

    back-end and front-end of app

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using my_app2.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_app2.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_app2.Models
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string Content { get; set; }
+    }
+}
